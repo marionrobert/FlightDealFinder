758a61393cbdcb8a29b63bfa2e1d6f0fe66a0c3a
Marion <mrobert148@gmail.com>
2023-03-10 12:58:51 +0100

    search for iatacode with kiwi-tequila api

--- flight_search.py
+++ flight_search.py
@@ -1,14 +1,24 @@
 import requests
+import os
+
+TEQUILA_ENDPOINT = "https://api.tequila.kiwi.com/locations/query"
+TEQUILA_API_KEY = os.environ["TEQUILA_API_KEY"]
+TEQUILA_HEADERS = {"apikey": TEQUILA_API_KEY}
 
 
 class FlightSearch:
 
     def get_destination_code(self, city_name):
-        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
-        code = "TESTING"
+        tequila_params = {
+            "term": city_name,
+            "locale": "en-US",
+            "location_types": "city",
+        }
+        response = requests.get(url=TEQUILA_ENDPOINT, headers=TEQUILA_HEADERS, params=tequila_params)
+        response.raise_for_status()
+        results = response.json()["locations"]
+        code = results[0]["code"]
         return code
-
-
 
 
 
